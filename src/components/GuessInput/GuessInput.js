f34b73714c2545bd98853736a56e0e4d0f4497e6
ac-rob <a_roberts252@hotmail.co.uk>
2024-10-21 11:08:34 +0100

    get input working

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,15 +1,41 @@
 import React from "react";
 
 function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  const handleInput = (event) => {
+    setGuess(event.target.value.toUpperCase());
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setGuess(guess);
+    console.log({ guess });
+    setGuess("");
+  };
   return (
     <div>
       <h2>HI THERE</h2>
-      <form className="guess-input-wrapper">
+      <form onSubmit={handleSubmit} className="guess-input-wrapper">
         <label htmlFor="guess-input">Enter guess:</label>
-        <input type="text" id="guess-input"></input>
+        <input
+          type="text"
+          id="guess-input"
+          pattern="^[A-Za-z]{5}$"
+          title="Enter exactly 5 letters."
+          value={guess}
+          onChange={handleInput}
+        ></input>
+        <button type="submit">Guess</button>
       </form>
     </div>
   );
 }
 
 export default GuessInput;
+
+// TODO
+// prevent default
+// Console log resuilt
+// track state
+// add function
