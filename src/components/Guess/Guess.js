39fcd5cbb5f9e76e5a7eef23d3f6526ee8bf6901
ac-rob <a_roberts252@hotmail.co.uk>
2024-10-28 09:35:54 +0000

    fix logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import { range } from '../../utils';
+import React from "react";
+import { range } from "../../utils";
 
 function Guess({ value }) {
-  let chars = [];
-  console.log(`${value} :value`);
-  
-  if (value) {
-    chars = value.split('');
-  } else {
-    chars = ['', '', '', '', ''];
-  }
-  
-  console.log(chars);
-  
   return (
     <p className="guess">
-      {range(5).map((cell, index) => (
-        <span className="cell" key={index}>
-          {chars[index]}
+      {range(5).map((num) => (
+        <span className="cell" key={num}>
+          {value ? value[num] : undefined}
         </span>
       ))}
     </p>
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
-import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+import { range } from "../../utils";
 import Guess from "../Guess/Guess";
 
 function GuessResults({ guessList }) {
   return (
     <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((num) => {     
-          return <Guess value={guessList[num]}/>
-})}
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => {
+        return <Guess value={guessList[num]} key={num} />;
+      })}
     </div>
   );
 }
